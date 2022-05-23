60a53c126f725eab3585786a1dfc0792133c7d39
anonymousDeveloper10 <solvgraph@hotmail.com>
2022-05-23 17:45:11 +0200

    LE RESULTAT EST IDENTIQUE ENTRE EXCEL SOLVEUR ET PYTHON PULP

--- 11. Solveurs linéaires Python/01. OPTIMISATIONS PAR CATEGORIES D UTILISATION/PRODUCTION PLANIFIEE MULTI PERIODE AVEC STOCKS/03. Production planifiée - Comparaison Excel - OK 100%.py
+++ 11. Solveurs linéaires Python/01. OPTIMISATIONS PAR CATEGORIES D UTILISATION/PRODUCTION PLANIFIEE MULTI PERIODE AVEC STOCKS/03. Production planifiée - Comparaison Excel - OK 100%.py
@@ -1,36 +1,10 @@
 """ 
  PLANIFICATION DE PRODUCTION AVEC STOCKS
- MINIMISATION DES COUTS
+ MINIMISATION DES COUTS 2022 
 
+COMPARAISON ENTRE LE SOLVEUR EXCEL ET PYTHON PULP = RESULTAT 100% IDENTIQUE AU FEUILLE 6 DU FICHIER EXCEL CI JOINT !
 
-Source : Taiwan University  : 2022 https://www.coursera.org/learn/operations-research-modeling/lecture/vW447/2-8-simple-lp-formulation-production-and-inventory
-Source 2 :  https://machinelearninggeek.com/solvingmulti-period-production-scheduling-problem-in-python-using-pulp/
-Source 3 : http://www.columbia.edu/itc/sipa/U6033/client_edit/lectures/lec4.pdf
-Source 4 : https://linuxtut.com/en/0d2a01c66230e0dc6fe9/ """
-
-
-""" Histoire : 
-Quand on veut faire des décisions, On veut se soucier de ce qui se passe dans le futur.
-Ici, on parle de problèmes Multi Périodes ( Dates multiples)
-
-Le plus souvent, les produits que l'on fait sont stockés et vendus plus tard.
-parfois, le prix futur varie, la production est plus moins chère, ou la capacité par jour n'est pas suffisante...
-
-Donc la décision de production doit être prise en onjonction avec la décision de l'inventaire.
-
-Si on prends un exmple sur 4 jours différents, on a donc 
-- la quantité à produire du fait de la demandee par jour
-- le côut de production par jour ( par exemple si c'est le dimanche , c'est plus cher de produire)
-- le coût de l'entretien des stocks est de 1 euro par jour par unité de produit vendu donc si on garde un produit 3 jours, le cout sera de 3 euros """
-
-# NOTE : Cette version utilise des boucles FOR à la place du premier exemple ou les vars de décision sont écrites en dur ! ELLE TROUVE LE MEME RESULTAT
-# Ce code fonctionne et retrouve le même résultat que http://www.columbia.edu/itc/sipa/U6033/client_edit/lectures/lec4.pdf sur le problème nuMéro 10
-""" HISTOIRE COMPLETE DE TAIWAN UNIVERSITY: 
-LE MANAGER DIT QUON VA VENDRE 100 , 150, 200 et 170 les jours 1 ,2 , 3 et 4 de la prochine semaine.
-LES COUTS DE PROD VARIENT EN FONCTION DES JOURS : 9e 12e 10e ET 12e les jours 1 ,2 , 3 et 4 de la prochine semaine.
-LE COUT DE STOCKAGE EST DE 1 E PAR OBJET STOCKE DONC SI ON LE GARDE 4 JOURS CA FAIT 4 EUROS DE COUT 
-QUEL EST LE MEILLEUR PLAN DE PRODUCTION POUR MINIMISER LES COUTS ET NE PAS AVOIR TROP DE STOCKS ?
-
+THE EXCEL SOLVER AND PYTHON PULP RESULTS ARE 100% IDENTICALS !!!
 """
 
 
@@ -40,9 +14,9 @@ from pulp import *
 # 1. On veut minimiser Les coûts de production, et d'inventaire.
 model = LpProblem("Minimiser le cout",LpMinimize)
 
-# ON définit les couts de production et de stockage pour les jours 1 2 3 ET 4 ; Dans quaters, c'est le nombre de jours SOIT 4. Define production cost, inventory cost, and demande.
+# ON définit les couts de production et de stockage pour les jours 1 2 3 ET 4 ; Dans periodes, c'est le nombre de jours SOIT 4. Define production cost, inventory cost, and demande.
 # Définir les couts de production par période, le cout de l'inventaire, et la demandee reportée par le service commercial
-quaters = list(range(6))
+periodes = list(range(6))
 cout_production=[240,250,265,285,280,260]
 cout_stockage=[3.60,3.75,3.98,4.28,4.20,3.90]
 demande=[1000,4500,6000,5500,3500,4000]
@@ -52,30 +26,31 @@ min_stock = [1500,1500,1500,1500,1500,1500] # Par sécurité on garde 1500 objet
 max_stock = [6000,6000,6000,6000,6000,6000]
 
 # 2. Définir les variables de décision : Production et Stock - Define Decision Variables: Production and Inventory
-x = LpVariable.dicts('quater_prod_', quaters,lowBound=0, cat='Continuous')
-y = LpVariable.dicts('quater_stock_', quaters,lowBound=0, cat='Continuous')
+x = LpVariable.dicts('production_', periodes,lowBound=0, cat='Continuous')
+y = LpVariable.dicts('stock_', periodes,lowBound=0, cat='Continuous')
 
 
 # 3. Définir l'objectif , On veut minimiser les coûts de production + de stockage comme dans le problème de université taiwan
-model += lpSum([cout_production[i]*x[i] for i in quaters]) + lpSum([cout_stockage[i]*y[i] for i in quaters])
+model += lpSum([cout_production[i]*x[i] for i in periodes]) + lpSum([cout_stockage[i]*y[i] for i in periodes])
 
 # Définir les contraintes
 # Constrainte de capacité de production (Production-capacity constraints)
-for i in quaters:
+for i in periodes:
     model.addConstraint(x[i]<=max_production[i])
-for i in quaters:
+for i in periodes:
     model.addConstraint(x[i]>=min_production[i])
 
-for i in quaters:
+# Constrainte de stocks min et max
+for i in periodes:
     model.addConstraint(y[i]<=max_stock[i])
-for i in quaters:
+for i in periodes:
     model.addConstraint(y[i]>=min_stock[i])
 
 # Contrainte de balance de stocks ( Inventory-balance constraints)
-model.addConstraint(x[0] - (y[0] - 2750) == demande[0]) # (Month 1)
+model.addConstraint(x[0] - (y[0] - 2750) == demande[0]) # (Mois 1)
 
-for i in quaters[1:]:
-    model.addConstraint(x[i] - y[i] + y[i-1] == demande[i]) # par (jour 2, 3, 4) 
+for i in periodes[1:]:
+    model.addConstraint(x[i] - y[i] + y[i-1] == demande[i]) # par mois 1 , 2 , 3 , 4 , 5 , 6
 
 #ON résouds avec le solveur pulp ou un autre entre parenthèses
 model.solve()
