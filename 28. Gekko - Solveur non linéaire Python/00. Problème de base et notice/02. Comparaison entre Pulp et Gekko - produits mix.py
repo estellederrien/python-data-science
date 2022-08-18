41442273cac6d2d1a133cc256a814110774ad77e
anonymousDeveloper10 <solvgraph@hotmail.com>
2022-08-19 00:29:57 +0200

    fix

--- 28. Gekko - Solveur non linéaire Python/00. Problème de base et notice/02. Comparaison entre Pulp et Gekko - produits mix.py
+++ 28. Gekko - Solveur non linéaire Python/00. Problème de base et notice/02. Comparaison entre Pulp et Gekko - produits mix.py
@@ -1,49 +1,43 @@
-""" 
-https://stackoverflow.com/questions/73409787/gekko-and-pulp-results-differs-but-both-are-valid-why
-Gekko and Pulp results differs but both are valid, why?
-Asked today
-Modified today
-Viewed 23 times
-0
 
-I have this simple product mix linear program, Pulp and Gekko results differs but results are both valid.
 
-I wonder why and how should I get exactly the same result using both solvers ?
+# I have this simple product mix linear program, Pulp and Gekko results differs a lot but results are both valid.
 
-Let's say x1 is the amount of car 1, and x2 the amount of car 2, for example (A simple product mix linear program)... I need to know which cars I need to construct, to maximize my profit.
- """
-    from gekko import GEKKO
-    m = GEKKO(remote=False) 
-    m.options.SOLVER = 1
-    
-    # Initialize variables
-    x1 = m.Var(value=1,lb=0,ub=1000,integer=True)
-    x2 = m.Var(value=1,lb=0,ub=1000,integer=True)
-    
-    # Equations
-    m.Equation(2 * x1 + 3 * x2 <= 800)
-    m.Equation(2 * x1 + x2 <= 500)
-    
-    # Objective
-    m.Maximize( 2 * x1 + 1 * x2) 
-    
-    # Steady state optimization
-    m.options.IMODE = 3 
-    
-    # Solve
-    m.solve(disp=False) 
-    
-    print('Results')
-    print('x1: ' + str(x1.value))
-    print('x2: ' + str(x2.value))
-    print('Objective: ' + str(m.options.objfcnval))
-    
-    Results
-    x1: [184.0]
-    x2: [132.0]
-    Objective: -500.0
+# I wonder why and how should I get exactly the same result using both solvers ?
 
-# And, next, this is my Python Pulp code, You will notice that Python Pulp only choose the x1 variable, while Gekko seems to balance between the x1 and the x2 variables ; both results are valid, and constraints does not get exceeded:
+# Let's say x1 is the amount of car toy 1, and x2 the amount of car toy 2, for example (You know... A simple product mix linear program)... I want to know which cars to build to maximize my profit.
+
+from gekko import GEKKO
+m = GEKKO(remote=False) 
+m.options.SOLVER = 1
+
+# Initialize variables
+x1 = m.Var(value=1,lb=0,ub=1000,integer=True)
+x2 = m.Var(value=1,lb=0,ub=1000,integer=True)
+
+# Equations
+m.Equation(2 * x1 + 3 * x2 <= 800)
+m.Equation(2 * x1 + x2 <= 500)
+
+# Objective
+m.Maximize( 2 * x1 + 1 * x2) 
+
+# Steady state optimization
+m.options.IMODE = 3 
+
+# Solve
+m.solve(disp=False) 
+
+print('Results')
+print('x1: ' + str(x1.value))
+print('x2: ' + str(x2.value))
+print('Objective: ' + str(m.options.objfcnval))
+
+Results
+x1: [184.0]
+x2: [132.0]
+Objective: -500.0
+
+# And, next, this is my Python Pulp code. You will notice that Python Pulp only picks the x1 variable, while Gekko seems to balance between the x1 and the x2 variables; both results are valid, and constraints are not exceeded:
 
 import pulp 
   
@@ -72,6 +66,10 @@ x1 = 250.0
 x2 = 0.0
 500.0
 
+# Gekko tells me to build 184 "car toy 1" and 132 "car toy 2" to maximize my profit, but Pulp tells me to only build 250 "car toy 1" , and to ignore "car toy 2".
+
 # Can somebody please tell me if that is a normal behavior, and how should I get the same behavior on both solvers ?
+
+# Is is due to a different simplex or interior point algorithm used by theses libraries?
 
 # ( Secondary question : I don't know why the Gekko objective result is negative ... )
